666c68587bf4f4c09e2143614421af0012fc73c2
S D Rausty <27742457+SDRausty@users.noreply.github.com>
2019-09-22 12:06:20 -0400

    Update ma.bash

--- ma.bash
+++ ma.bash
@@ -38,15 +38,16 @@ trap _SMATRPSIGNAL_ HUP INT TERM
 trap _SMATRPQUIT_ QUIT 
 
 cd "$JDR"
-# _AT_ arpitjain099/Android-Accelerometer-Compass-GPS-sensors-data-logging 13845848c7fe3cbb542b3bed805c25c23a4dba28
+_AT_ arpitjain099/Android-Accelerometer-Compass-GPS-sensors-data-logging 13845848c7fe3cbb542b3bed805c25c23a4dba28
+_AT_ burhanaras/Compass c974a16f1c4076a939bc0cefdf04d8e79fdff116
 _AT_ farsitel/android_packages_apps_QiblaCompass d30afb971dbac8f49cf0b438572832d56cc32d12
 _AT_ iliu/Android-Compass c570a99cb37b9f100de83eac8a1ffc9bac1b9557
 _AT_ jarofgreen/Just-A-Damn-Compass 32e3d60ecadac260f71da17a6cb4b22084c5bcc2
 _AT_ JosephChenEcho/Android-Compass 88d1bd8e32a118600e3fd229defd87709b600795
 _AT_ JosephChenEcho/Compass-V1.0 ee900c23f0af1a8153bcba213beb4e2f444dd165
 _AT_ ldo/3D-Compass 3583e0490cd9c8a6a42e4507451e220c359b8405
-# _AT_ oliverskawronek/compass-navigator 712a2192352dab13940a9cddaae362126c7e222c
+_AT_ oliverskawronek/compass-navigator 712a2192352dab13940a9cddaae362126c7e222c
 _AT_ phishman3579/android-compass 6fada1435b3dca1f30e72233ff05ae516cdd01e4
-# _AT_ umair13adil/DirectionMeter 5975dd049f511992698bdd1ae51654824c177010
+_AT_ umair13adil/DirectionMeter 5975dd049f511992698bdd1ae51654824c177010
 
-#ma.bash OEF
+# OEF ma.bash
