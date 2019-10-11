11d3819bd9c29a03419366a2de575bea959fae23
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-10-11 07:02:02 -0400

    new file:   do.sums.bash 	deleted:    do.trees.bash 	modified:   ma.bash 	renamed:    trees.md5sum.sum -> ztree.md5.sum 	renamed:    trees.sha1sum.sum -> ztree.sha1.sum 	renamed:    trees.sha224sum.sum -> ztree.sha224.sum 	renamed:    trees.sha256sum.sum -> ztree.sha256.sum 	renamed:    trees.sha384sum.sum -> ztree.sha384.sum 	renamed:    trees.sha512sum.sum -> ztree.sha512.sum

--- ma.bash
+++ ma.bash
@@ -42,9 +42,8 @@ _AT_ iliu/Android-Compass c570a99cb37b9f100de83eac8a1ffc9bac1b9557
 _AT_ jarofgreen/Just-A-Damn-Compass 32e3d60ecadac260f71da17a6cb4b22084c5bcc2
 _AT_ JosephChenEcho/Android-Compass 88d1bd8e32a118600e3fd229defd87709b600795
 _AT_ JosephChenEcho/Compass-V1.0 ee900c23f0af1a8153bcba213beb4e2f444dd165
-_AT_ ldo/3D-Compass 3583e0490cd9c8a6a42e4507451e220c359b8405
-#_AT_ oliverskawronek/compass-navigator 712a2192352dab13940a9cddaae362126c7e222c
+# _AT_ ldo/3D-Compass 3583e0490cd9c8a6a42e4507451e220c359b8405
+_AT_ oliverskawronek/compass-navigator 712a2192352dab13940a9cddaae362126c7e222c
 _AT_ phishman3579/android-compass 6fada1435b3dca1f30e72233ff05ae516cdd01e4
 #_AT_ umair13adil/DirectionMeter 5975dd049f511992698bdd1ae51654824c177010
-
 # ma.bash OEF 
