b1a3f94adf6606d3ebccc3dab6187f9eda3859f9
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-01-11 00:00:58 -0500

    commit 852461629 in 20200111

--- ma.bash
+++ ma.bash
@@ -11,7 +11,7 @@ cd "$JDR"
 # _AT_ arpitjain099/Android-Accelerometer-Compass-GPS-sensors-data-logging 13845848c7fe3cbb542b3bed805c25c23a4dba28
 _AT_ 7heaven/Compass 3828eed9834b31df551997d5918eea805014a007
 _AT_ burhanaras/Compass c974a16f1c4076a939bc0cefdf04d8e79fdff116
-_AT_ farsitel/android_packages_apps_QiblaCompass d30afb971dbac8f49cf0b438572832d56cc32d12
+# _AT_ farsitel/android_packages_apps_QiblaCompass d30afb971dbac8f49cf0b438572832d56cc32d12
 _AT_ iliu/Android-Compass c570a99cb37b9f100de83eac8a1ffc9bac1b9557
 _AT_ jarofgreen/Just-A-Damn-Compass 32e3d60ecadac260f71da17a6cb4b22084c5bcc2
 _AT_ JosephChenEcho/Android-Compass 88d1bd8e32a118600e3fd229defd87709b600795
